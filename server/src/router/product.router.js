871f3c8ae9a71e1fb3f2403876b447db36fc4ecd
Heba-Adwan <hebawael1575@gmail.com>
2023-05-22 11:11:12 +0300

    filter By Price in Backend Relates #16 Closes #16

--- server/src/router/product.router.js
+++ server/src/router/product.router.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { createProduct } from '../controller/products/product.js'
-import { getAllProductsController } from '../controller/products/getAllProductsController.js';
+import { getAllProductsController } from '../controller/products/getAllProductsController.js'
+import { filterProductsByPriceController } from '../controller/products/filterProductsByPriceController.js';
 
 const productRouter = express.Router()
 
 productRouter.get('/products', getAllProductsController)
+productRouter.get('/products/:price', filterProductsByPriceController)
 productRouter.post('/product', createProduct)
 
 export default productRouter
