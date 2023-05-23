80f7b4d1001734f6f55703241b1afbc5a33f7349
MohammedSallout <mzsalloot@gmail.com>
2023-05-23 16:26:13 +0300

    fix any

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,8 @@ import { useState } from 'react'
 import SignUp from './pages/Signup'
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
-<<<<<<< HEAD
 import Navbar from './components/Navbar'
-=======
 import Seller from './pages/seller'
->>>>>>> d147bdc8d4bd723c8211b07b69a41c0175153e7a
 
 
 
@@ -14,19 +11,11 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-    <Navbar />
-
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signUp" element={<SignUp />} />
-=======
 
       <Routes>
       {/* <Route path="/" element={<Home />} />
       <Route path="/signUp" element={<SignUp />} /> */}
         <Route path="/" element={<Seller />} />
->>>>>>> d147bdc8d4bd723c8211b07b69a41c0175153e7a
       </Routes>
     </div>
   );
