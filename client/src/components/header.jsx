14a28d0eab9ae9bfb04b5af73c5be2476356f0de
Heba-Adwan <hebawael1575@gmail.com>
2023-05-23 15:48:22 +0300

    get all products front Relates #54 Closes #54

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
 
-
 const HeaderHome=()=>{
 return(
   <>
   <div className='head_image'>
-
+<img src='https://media.istockphoto.com/id/1280247722/photo/woman-shopping-at-black-friday.jpg?b=1&s=170667a&w=0&k=20&c=3GVQmKFOxzBQgYpk4mf_gU8kDDix7g1K3jVEhwTr0nI=' alt='headImage'/>
   </div>
+
   <div className='head_content'>
 <h1>Welcome in ecomerce shop</h1>
 <p>The right place to shop, here we offer you diffrent types of devices such as i-phones, laptops,  tablets. <span>What are you waiting for!</span>  </p>
