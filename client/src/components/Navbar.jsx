ae8ad367111b852bce8fccaff6036698d5b31fdb
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-05-25 16:00:06 +0300

    get user data and token to check auth Relate #98 closes #98

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
 import '../style/navbar.css'
 function Navbar() {
+  const auth = JSON.parse(localStorage.getItem('user'))
+  console.log(auth);
   return (
     <nav className="Navbar">
       <h1 className="logo">AMH Store</h1>
       <ul className="list">
         <li><Link to="/">Home</Link></li>
         <li><Link to="products">Shop</Link></li>
-        <li><Link to="seller">Seller</Link></li>
+        {auth.user ? <li><Link to="seller">Seller</Link></li> : null}
       </ul>
       <div className="side">
         <Link to="signin" className="login">
