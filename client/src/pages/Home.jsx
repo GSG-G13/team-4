b6cdca95f64345c151af62a53b111047498f0b19
MohammedSallout <mzsalloot@gmail.com>
2023-05-25 09:02:54 +0300

    commit change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import '../style/home.css'
+import "../style/home.css";
 import HeaderHome from "../components/header";
 
-
-const Home=()=>{
+const Home = () => {
   const [data, setData] = useState([]);
   const byHome = async () => {
-    const { data } = await axios.get("/api/")
-    setData([...data])
-  }
+    const { data } = await axios.get("/api/");
+    setData([...data]);
+  };
 
   useEffect(() => {
-    byHome()
-  }, [])
+    byHome();
+  }, []);
 
-
-return(
-  <>
-  <HeaderHome/>
-  <div className="products-home">
-    <h2>View Products</h2>
-    <div className="productItems">
-    {data.map((product) => {
-    return(
-      <div className="product-home" key={product.id}>
-  <img src={product.image} alt="img" />
-  <h4>{product.title}</h4>
-  </div>
-    )
-  })}
-    </div>
-
-  </div>
-  </>
-)
-}
-
+  return (
+    <>
+      <HeaderHome />
+      <div className="products-home">
+        <h2>View Products</h2>
+        <div className="productItems">
+          {data.map((product) => {
+            return (
+              <div className="product-home" key={product.id}>
+                <img src={product.image} alt="img" />
+                <h4>{product.title}</h4>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
